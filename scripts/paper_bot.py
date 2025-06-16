0981d9b81c18748b790ce3412300725e4bb4f5a3
3080Ti <3080ti.com>
2025-06-17 06:01:46 +1000

    update

--- scripts/paper_bot.py
+++ scripts/paper_bot.py
@@ -1,111 +1,124 @@
 import requests
 import gradio as gr
 from langchain import hub
-from semanticscholar import SemanticScholar
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from semanticscholar import SemanticScholar 
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.agents import Tool, AgentExecutor, create_react_agent
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
 from langchain.prompts import PromptTemplate
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_openai.chat_models import ChatOpenAI
-from langchain_community.retrievers.arxiv import ArxivRetriever
-from langchain_community.document_loaders.pdf import OnlinePDFLoader
+from langchain_community.retrievers import ArxivRetriever 
+from langchain_core.vectorstores import VectorStoreRetriever
+from langchain.chains.summarize import load_summarize_chain
+import os
+from PyPDF2 import PdfReader
 
-arxiv_tool = ArxivRetriever(top_k_results=5)
 client = SemanticScholar()
 llm = ChatOpenAI(
     model="gpt-4o-mini", 
     streaming=True, 
-    callbacks=StreamingStdOutCallbackHandler()
+    callbacks=[StreamingStdOutCallbackHandler()]
 )
 react_agent_prompt = hub.pull("hwchase17/react")
 retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
 
-def fetch_metadata(paper_id: str) -> str:
-    base_url = "https://api.semanticscholar.org/graph/v1/paper/"
-    url = f"{base_url}ARXIV:{paper_id}?fields=title,abstract,authors,citationCount"
-    headers = {}
-    api_key = ""
-    if api_key:
-        headers["x-api-key"] = api_key
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Raise HTTPError for bad responses (4xx or 5xx)
-        data = response.json()
-        return data
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching data: {e}")
-        return None
+def search_arxiv(query: str):
+    arxiv_retriever = ArxivRetriever(load_max_docs=1, load_all_available_meta=True, get_full_documents=True)
+    results = arxiv_retriever.invoke(query)
+    paper = {"title": results.metadata['Title'], "date": results.metadata['Published'], "pdf_url": results.metadata['links'][-1]}
+    return paper
 
-# def get_bibtex(paper_id: str) -> str:
-#     paper = client.get_paper(paper_id)
-#     return paper.citationStyles["bibtext"]
+def download_pdf(paper: dict, save_dir: str = "downloads"):
+    pdf_url = paper['pdf_url']
+    if not os.path.exists(path=save_dir):
+        os.makedirs(save_dir)
+    local_path = os.path.join(save_dir, pdf_url.split('/')[-1]+".pdf")
+    resp = requests.get(pdf_url, timeout=30)
+    with open(local_path, "wb") as f:
+        f.write(resp.content)
+    return local_path
 
-def web_chat(text: str) -> str:
-    return llm.invoke(text).content
+def parse_pdf(pdf_path: str):
+    reader = PdfReader(pdf_path)
+    pages = [page.extract_text() or "" for page in reader.pages]
+    return "\n".join(pages)
 
-pdf_retriever = None
-
-def load_pdf_and_embed(pages): 
+def get_retriever(pages): 
     embeddings = OpenAIEmbeddings()
     vector_index = FAISS.from_documents(pages, embeddings)
-    pdf_retriever = vector_index.as_retriever(search_kwargs={"k": 3})
-    return pdf_retriever
+    retriever = vector_index.as_retriever(search_kwargs={"k": 3})
+    return retriever
 
-def run_rag(query: str) -> str:
-    url = f"https://arxiv.org/pdf/{paper_id}"
-    splitter = RecursiveCharacterTextSplitter(
-        chunk_size = 1000,
-        chunk_overlap = 100
-    )
-    pages = OnlinePDFLoader(url).load_and_split(splitter)
-    pdf_retriever = load_pdf_and_embed(pages)
+def answer_question(question: str, retriever: VectorStoreRetriever):
     combine_docs_chain = create_stuff_documents_chain(llm, retrieval_qa_chat_prompt)
-    rag_chain = create_react_agent(pdf_retriever, combine_docs_chain)
-    result = rag_chain({"query": query})
-    return result["answer"]
+    rag_chain = create_react_agent(retriever, combine_docs_chain)
+    result = rag_chain({"query": question})
+    return result['answer']
+
+def summarize_paper(question: str, retriever: VectorStoreRetriever):
+    docs = retriever.get_relevant_documents(question)
+    summarizer = load_summarize_chain(llm=llm, chain_type="map_reduce")
+    return summarizer.run(docs)
+
+def make_answer_tool(retriever):
+    return Tool(
+        name="answer_question",
+        func=lambda q: answer_question(q, retriever),
+        description="Answer based on querying the document's vector index"
+    )
+
+def make_summarize_tool(retriever):
+    return Tool(
+        name="summarize_paper",
+        func=lambda _: summarize_paper(_, retriever),
+        description="Summarize the paper in its vectors store"
+    )
 
 tools = [
-    arxiv_tool,
     Tool.from_function(
-        fetch_metadata, 
-        name="PaperMetadataTool", 
-        description="Fetch metadata from Semantic Scholar"
+        search_arxiv,
+        name="Search arXiv", 
+        description="Search arXiv for papers based on question and required number of papers will be searched for, and return title, published data, and pdf link"
     ),
-    # Tool.from_function(
-    #     get_bibtex, 
-    #     name="BibTeXTool", 
-    #     description="Get BibTeX citation"
-    # ),
     Tool.from_function(
-        fetch_metadata, 
-        name="ChatTool", 
+        download_pdf, 
+        name="Download pdf", 
+        description="Download pdf searched from arxiv and return the local path where the pdf is downloaded at"
+    ), 
+    Tool.from_function(
+        parse_pdf, 
+        name="Parse pdf", 
         description="General chat"
-    ),
-    Tool.from_function(
-        func=run_rag,
-        name="RAGTool",
-        description="Use this to answer questions from the uploaded paper"
-    )
+    ) 
 ]
 
-cot_template = PromptTemplate(
-    input_variables=["chat_history", "input", "agent_scratchpad"],
-
-    template="""
+cot_template = PromptTemplate.from_template( 
+    """
     You are PaperBuddy, an AI research assistant that thinks step by step.\n
-    {chat_history}\n
-    User: {input}\n
-    Thought: I will break down the problem into substeps.\n
-    {agent_scratchpad}\n
+    Answer the following questions as best you can. You have access to the following tools:
+    {tools}
+    Use the following format:
+    Question: the input question you must answer
+    Thought: you should always think about what to do
+    Action: the action to take, should be one of [{tool_names}]
+    Observation: the result of the action
+    ... (this Thought/Action/Action Input/Observation can repeat N times)
+    Thought: I now know the final answer
+    Final Answer: the final answer to the original input question
+
+    Begin!
+
+    Question: {input}
+    Thought:{agent_scratchpad}\n
     """
 )
-agent = create_react_agent(llm, tools, cot_template)
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, max_iterations=5)
+agent_ = create_react_agent(llm, tools, cot_template)
+agent = AgentExecutor(agent=agent_, tools=tools, verbose=True, max_iterations=5)
 
-# def chat_interface(user_input, chat_history):
-#     if "pdf:" in user_input.lower() and pdf_retriever:
-#         rag = 
-
+papers = search_arxiv("chain-of-thought prompting survey")
+print("Arxiv Search Results:")
+for paper in papers: 
+    print(parse_pdf(download_pdf(paper['pdf_url'])))
+  
